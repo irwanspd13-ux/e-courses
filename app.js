63a1ae9ab59a24864ff12d92caec3d4ba930d12c
Irwan Bin Abdullah <irwanspd13@guru.smp.belajar.id>
2026-03-01 07:14:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,16 @@
 // KONFIGURASI SUPABASE KLIEN (app.js)
 // ==========================================
 
-// 1. Kredensial Supabase Anda
 const SUPABASE_URL = 'https://nieclaypsuyhyskbfftf.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5pZWNsYXlwc3V5aHlza2JmZnRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMDI3OTYsImV4cCI6MjA4Nzg3ODc5Nn0.Y_tREg1mREpVogiCgKLr-z8pU4LjpmVkIDCgXtU0OWg';
 
-// 2. Inisialisasi Supabase Client
-// (window.supabase berasal dari link CDN yang akan kita pasang di HTML nanti)
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Gunakan 'window.supabaseClient' agar variabel ini bisa dibaca oleh index.html dan admin.html
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// 3. Fungsi untuk mengecek koneksi (Bisa dilihat di Inspect Element -> Console)
+// Fungsi untuk mengecek koneksi
 async function checkSupabaseConnection() {
     try {
-        const { data, error } = await supabase.from('courses').select('id').limit(1);
+        const { data, error } = await window.supabaseClient.from('courses').select('id').limit(1);
         if (error) throw error;
         console.log("✅ Berhasil terhubung ke database Supabase EduPro!");
     } catch (err) {
@@ -21,5 +19,5 @@ async function checkSupabaseConnection() {
     }
 }
 
-// Jalankan tes koneksi saat file ini dimuat
+// Jalankan tes koneksi
 checkSupabaseConnection();
